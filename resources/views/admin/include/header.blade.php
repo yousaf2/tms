93f764b5acee2bbc0278e0f8fd73dc078b9379ce
yousaf shafqat <yshafqat0@gmail.com>
2021-11-16 15:42:56 +0500

    =db table changes

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -965,14 +965,14 @@ License: You must have a valid license purchased only from themeforest(the above
                 <!-- BEGIN: Top Bar -->
                 <div class="top-bar">
                     <!-- BEGIN: Breadcrumb -->
-                    <div class="-intro-x breadcrumb mr-auto hidden sm:flex"> <a href="" class="">Application</a> <i data-feather="chevron-right" class="breadcrumb__icon"></i> <a href="" class="breadcrumb--active">Dashboard</a> </div>
+                    <div class="-intro-x breadcrumb mr-auto hidden sm:flex"> <!--<a href="" class="">Application</a>--> <i data-feather="chevron-right" class="breadcrumb__icon"></i> <a href="{{url('admin/')}}" class="breadcrumb--active">Dashboard</a> </div>
                     <!-- END: Breadcrumb -->
                     <!-- BEGIN: Search -->
                     <div class="intro-x relative mr-3 sm:mr-6">
-                        <div class="search hidden sm:block">
+                        <!-- <div class="search hidden sm:block">
                             <input type="text" class="search__input input placeholder-theme-13" placeholder="Search...">
                             <i data-feather="search" class="search__icon dark:text-gray-300"></i> 
-                        </div>
+                        </div> -->
                         <a class="notification sm:hidden" href=""> <i data-feather="search" class="notification__icon dark:text-gray-300"></i> </a>
                         <div class="search-result">
                             <div class="search-result__content">
@@ -1138,14 +1138,14 @@ License: You must have a valid license purchased only from themeforest(the above
                         <div class="dropdown-box w-56">
                             <div class="dropdown-box__content box bg-theme-38 dark:bg-dark-6 text-white">
                                 <div class="p-4 border-b border-theme-40 dark:border-dark-3">
-                                    <div class="font-medium">Russell Crowe</div>
-                                    <div class="text-xs text-theme-41 dark:text-gray-600">DevOps Engineer</div>
+                                    <div class="font-medium">{{Auth::user('web')->first_name}} {{Auth::user('web')->last_name}}</div>
+                                    <!-- <div class="text-xs text-theme-41 dark:text-gray-600">DevOps Engineer</div> -->
                                 </div>
                                 <div class="p-2">
                                     <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="user" class="w-4 h-4 mr-2"></i> Profile </a>
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="edit" class="w-4 h-4 mr-2"></i> Add Account </a>
+                                    <!-- <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="edit" class="w-4 h-4 mr-2"></i> Add Account </a> -->
                                     <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="lock" class="w-4 h-4 mr-2"></i> Reset Password </a>
-                                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a>
+                                    <!-- <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a> -->
                                 </div>
                                 <div class="p-2 border-t border-theme-40 dark:border-dark-3">
                                     <a onclick="event.preventDefault(); document.getElementById('logout-form').submit();" href="{{ route('logout') }}" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 dark:hover:bg-dark-3 rounded-md"> <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
